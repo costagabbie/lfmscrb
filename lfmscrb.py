f9450df6f0cf967e955a7090255497c32e36eb5f
Gabriela Costa <102681995+costagabbie@users.noreply.github.com>
2022-09-28 22:28:38 -0300

    Fixed that pile of junk that i called code
    With a new Paramore song out, i decided to be a decent person and fix that junk and upload it fixed here.
    Added some features, check on readme

--- lfmscrb.py
+++ lfmscrb.py
@@ -6,16 +6,15 @@
 #    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
-
-
+import os
 import csv
 import pylast
 import time
+import argparse
 from math import floor
-from colorama import init, Fore, Back, Style
 from datetime import datetime
 
-API_KEY="REPLACE WITH YOUR API KEY" 
+API_KEY="REPLACE WITH YOUR API KEY"
 API_SECRET="REPLACE WITH YOUR API SECRET"
 username = "REPLACE WITH YOUR USERNAME"
 password_hash = pylast.md5("REPLACE WITH YOUR PASSWORD")
@@ -32,48 +31,58 @@ network = pylast.LastFMNetwork(
 # title,artist,album,length in seconds
 #
 
-init(autoreset=True)
-print(Style.BRIGHT + Back.BLACK + Fore.CYAN+"costagabbie's Last.fm Auto Scrobbler version 1.0.0.0\n")
-
 def main():
     #Check if the spam switch is present
-    if sys.argv[1] == "-s":
-        spam = True
-        print(Style.BRIGHT + Back,BLACK + Fore.RED+"Starting in spam mode, scrobbles will be done as fast as possible!\n")
-    else:
-        spam : False
-    print(Style.BRIGHT + Back.BLACK + Fore.CYAN+"[INFO]"+Style.BRIGHT + Back.BLACK + Fore.WHITE+"Trying to open trackinfo.csv.\n")
+    print(f"[INFO] Starting scrobbler at {datetime.utcnow()}")
+    parser = argparse.ArgumentParser(description="costagabbie's last.fm scrobbler v:1.1")
+    parser.add_argument('-s', '--spam', action= 'store_true',dest='spam', help='Wait just long enough to have a valid scrobble.',default=False)
+    parser.add_argument('-p', '--playlist', dest='playlist', help='playlist file in csv format.',default='tracklist.csv', type=str)
+    parser.add_argument('-l', '--loop', dest='loop', help='how many time the playlist should be looped, 0 means infinite.',default=0, type=int)
+    args= parser.parse_args()
+    iterCount = 0
+    print(f"[INFO] Trying to open {args.playlist}.")
     #Opening the tracklist
-    csv_file = open('tracklist.csv', 'r') 
-    reader = csv.reader(csv_file,delimiter=';')
-    # Going through each song entry in the tracklist
-    for row in reader:
-        # If the song is less than 30 seconds it don't count as scrobble so we go to the next song
-        if row[3] < 30:
-            print(Style.BRIGHT + Back,BLACK + Fore.YELLOW + "[WARNING] This song is shorter than 30 seconds, so it can't be scrobbled! \n")
-            continue
-        # Get the time that we started to play the song
-        timestamp = time.time()
-        # Prepare for sending a Now Playing request
-        lastfm_user = network.get_user(username)
-        if spam:
-            # If we are in spam mode, we should wait for at least half of the song length to scrobble
-            print(Style.BRIGHT + Back.BLACK + Fore.GREEN+"[INFO]"+Fore.WHITE+'Now Playing '+row[0]+'-'+row[1]+'(Album: '+row[2]+')')
-            # Sending the now playing request
-            network.update_now_playing(row[1],row[0],row[2])
-            print(Style.BRIGHT + Back.BLACK + "[INFO]"+Style.BRIGHT + Back.BLACK + Fore.WHITE+"Waiting for half of "+row[3]+" seconds to scrobble.\n")
-            # Waiting for half of the song + 1 second
-            time.sleep(floor(int(row[3])/2)+1)
+    
+    while(True):
+        print(f"[Info] Iteration {iterCount+1} starting at {datetime.utcnow()}")    
+        # Going through each song entry in the tracklist
+        if os.path.exists(args.playlist):
+            csv_file = open('tracklist.csv', 'r') 
+            reader = csv.reader(csv_file,delimiter=';')
         else:
-            # We are not in spam mode so we wait the full length of the song before scrobble
-            print(Style.BRIGHT + Back.BLACK + Fore.GREEN+"[INFO]"+Fore.WHITE+'Now Playing '+row[0]+'-'+row[1]+'(Album: '+row[2]+')')
-            network.update_now_playing(row[1],row[0],row[2])
-            print(Style.BRIGHT + Back.BLACK + "[INFO]"+Style.BRIGHT + Back.BLACK + Fore.WHITE+"Waiting for "+row[3]+" seconds to scrobble.\n")
-            time.sleep(int(row[3]))
-        # Now that we 'listened' to the song (or at least half of it on spam mode) we can scrobble
-        print(Style.BRIGHT + Back.BLACK + Fore.CYAN+"[INFO]"+Style.BRIGHT + Back.BLACK + Fore.WHITE+"Trying to scrobble "+title+"-"+artist+"("+row[2]+") at "+str(timestamp)+".\n")
-        network.scrobble(artist=row[1], title=row[0], timestamp=timestamp,album=row[2])
-    # We played the whole tracklist so bye bye.
-    print(Style.BRIGHT + Back.BLACK + Fore.CYAN+"[INFO]"+Style.BRIGHT + Back.BLACK + Fore.WHITE+"All done, exiting now. \n")
+            print(f"[ERROR] Cannot find the file {args.playlist}")
+            exit()
+        for row in reader:
+            # If the song is less than 30 seconds it don't count as scrobble so we go to the next song
+            if int(row[3]) < 30:
+                print(f"[WARNING]{row[0]}={row[1]} is shorter than 30 seconds({row[3]} seconds), so it can't be scrobbled!")
+                continue
+            # Get the time that we started to play the song
+            timestamp = time.time()
+            # Prepare for sending a Now Playing request
+            network.get_user(username)
+            if args.spam:
+                # If we are in spam mode, we should wait for at least half of the song length to scrobble
+                print(f"[INFO] Now Playing {row[0]} - {row[1]} (Album: {row[2]}) at {datetime.utcnow()}")
+                # Sending the now playing request
+                network.update_now_playing(row[1],row[0],row[2])
+                print(f"[INFO] Waiting for {str(floor(int(row[3])/2)+1)} seconds to scrobble.")
+                # Waiting for half of the song + 1 second
+                time.sleep(floor(int(row[3])/2)+1)
+            else:
+                # We are not in spam mode so we wait the full length of the song before scrobble
+                print(f"[INFO] Now Playing {row[0]} - {row[1]} (Album: {row[2]})")
+                network.update_now_playing(row[1],row[0],row[2])
+                print(f"[INFO] Waiting for {row[3]} seconds to scrobble.")
+                time.sleep(int(row[3]))
+            # Now that we 'listened' to the song (or at least half of it on spam mode) we can scrobble
+            print(f"[INFO] Trying to scrobble {row[0]} - {row[1]} (Album: {row[2]}) at{str(timestamp)}")
+            network.scrobble(artist=row[1], title=row[0], timestamp=timestamp,album=row[2])
+        iterCount = iterCount +1
+        # If we are in a non-infinite mode and the iteration count is enough
+        if (args.loop > 0) and (iterCount >= args.loop):
+            # We played everything so bye bye.    
+            print("All done, exiting now.")
+            exit()
 if __name__ == '__main__':  
     main()
